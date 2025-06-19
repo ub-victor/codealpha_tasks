a7fb2dab8b01fe856d1f2d2e7d2971c39ed18ee8
ub-victor <victoireushindi371@gmail.com>
2025-06-19 21:08:11 +0200

    Initial commit

---
+++ Web-Development/Task1/script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // DOM Elements
+    const taskInput = document.getElementById('task-input');
+    const addTaskBtn = document.getElementById('add-task-btn');
+    const taskList = document.getElementById('task-list');
+    const filterBtns = document.querySelectorAll('.filter-btn');
+    const clearCompletedBtn = document.getElementById('clear-completed');
+    const tasksCount = document.getElementById('tasks-count');
+    console.log(filterBtns);
+})
